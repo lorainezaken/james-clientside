639162114b6abe151496ce16a663cf35015f145c
lorainezaken <lorainezaken@gmail.com>
2017-06-20 10:11:13 +0300

    james-clientside

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { GenreService } from './genre.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GenreService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  constructor(private genreService: GenreService) {}
+  
+  genres: string[]
+  ngOnInit() {
+    this.genreService.getGenres()
+      .then(genres => {
+        this.genres = genres;
+      })
+  }
 }
---
+++ src/app/genre.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class GenreService {
+    constructor(private http: Http) { }
+
+    getGenres(): Promise<string[]> {
+         return this.http.get("http://james-server.herokuapp.com/FunkGenres")
+             .toPromise()
+             .then(response => {
+                 return response.json().genres.map(genre => genre.genreTitle) as string[]
+             })
+             .catch(this.handleError);
+    }
+    
+    private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+    }
+}
