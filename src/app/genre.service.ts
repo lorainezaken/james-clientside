af0b6130eb29e6b4a7fceffca4d1eddead35bf9b
lorainezaken <lorainezaken@gmail.com>
2017-06-20 11:05:30 +0300

    genre service

--- src/app/genre.service.ts
+++ src/app/genre.service.ts
@@ -18,7 +18,7 @@ export class GenreService {
     }
     
     private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
+    console.error('An error occurred', error);  // for demo purposes only
     return Promise.reject(error.message || error);
     }
 }
